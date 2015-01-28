d029d61bd1b9c91fa6d613842cf6094f66c484a8
Evangelos Giataganas <e.giataganas@gmail.com>
2015-01-28 17:14:37 +0000

    test_after_reopening_dogs_can_both_wag_and_bark fixed

--- about_open_classes.rb
+++ about_open_classes.rb
@@ -23,8 +23,8 @@ class AboutOpenClasses < Neo::Koan
 
   def test_after_reopening_dogs_can_both_wag_and_bark
     fido = Dog.new
-    assert_equal "WOOF", fido.wag
-    assert_equal "HAPPY", fido.bark
+    assert_equal "HAPPY", fido.wag
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
