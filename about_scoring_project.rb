b728e6ee40649602a937d6fa8adccfa9cbd3d344
Evangelos Giataganas <e.giataganas@gmail.com>
2015-01-28 16:48:31 +0000

    implementation of score(dice) method

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -31,7 +31,32 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
   # You need to write this method
-  0
+  result = 0
+  
+  counts = Hash.new(0) 
+  
+  dice.each do |value|
+    counts[value] = 0 if counts[value].nil?
+    counts[value] += 1
+  end
+  
+  counts.each do |item,numFound|
+    if item == 1 && numFound >= 3 then
+      result += 1000
+      numFound -= 3
+    end
+
+    if item != 1 && numFound >= 3 then
+      result += item * 100
+      numFound -= 3
+    end
+    
+    result += 100 * numFound if item == 1 && numFound <= 2 
+    
+    result += 50 * numFound if item == 5 && numFound <=2
+  end
+  
+  result
 end
 
 class AboutScoringProject < Neo::Koan
